27043c24542bb0db818925cd5ca64313d6c827d7
pbw <326301416@qq.com>
2019-09-21 22:15:42 +0800

    完善路由菜单管理页面（50%）

--- src/views/router_menu/index.vue
+++ src/views/router_menu/index.vue
@@ -22,64 +22,43 @@
       <el-table-column prop="meta.noCache" label="禁止缓存" />
     </el-table>
     <el-dialog title="新增菜单路由" :visible.sync="addFormVisible">
-      <el-form ref="ruleForm" :model="ruleForm" :rules="rules" label-width="100px">
+      <el-form ref="addForm" :model="addForm" :rules="rules" label-width="100px">
+        <el-form-item label="路由名称" hidden prop="pid">
+          <el-input v-model="addForm.pid" />
+        </el-form-item>
         <el-form-item label="路由名称" prop="name">
-          <el-input v-model="ruleForm.name" />
+          <el-input v-model="addForm.name" />
         </el-form-item>
-        <el-form-item label="菜单名称" prop="name">
-          <el-input v-model="ruleForm.name" />
+        <el-form-item label="菜单名称" prop="meta.title">
+          <el-input v-model="addForm.meta.title" />
         </el-form-item>
-        <el-form-item label="访问地址" prop="name">
-          <el-input v-model="ruleForm.name" />
+        <el-form-item label="访问地址" prop="path">
+          <el-input v-model="addForm.path" />
         </el-form-item>
-        <el-form-item label="组件路径" prop="name">
-          <el-input v-model="ruleForm.name" />
+        <el-form-item label="组件路径" prop="component">
+          <el-input v-model="addForm.component" />
         </el-form-item>
-        <el-form-item label="重定向地址" prop="name">
-          <el-input v-model="ruleForm.name" />
+        <el-form-item label="重定向地址" prop="redirect">
+          <el-input v-model="addForm.redirect" />
         </el-form-item>
-        <el-form-item label="可见性" prop="delivery">
-          <el-switch v-model="ruleForm.delivery" active-text="隐藏" inactive-text="可见" />
+        <el-form-item label="排序" prop="sort">
+          <el-input v-model="addForm.sort" />
         </el-form-item>
-        <el-form-item label="默认展开" prop="delivery">
-          <el-switch v-model="ruleForm.delivery" active-text="开" inactive-text="关" />
+        <el-form-item label="可见性" prop="hidden">
+          <el-switch v-model="addForm.hidden" active-text="隐藏" inactive-text="可见" />
         </el-form-item>
-        <el-form-item label="缓存控制" prop="delivery">
-          <el-switch v-model="ruleForm.delivery" active-text="开" inactive-text="关" />
+        <el-form-item label="默认展开" prop="alwaysShow">
+          <el-switch v-model="addForm.alwaysShow" active-text="开" inactive-text="关" />
         </el-form-item>
-        <el-form-item label="面包屑状态" prop="delivery">
-          <el-switch v-model="ruleForm.delivery" active-text="显示" inactive-text="不显示" />
+        <el-form-item label="缓存控制" prop="meta.noCache">
+          <el-switch v-model="addForm.meta.noCache" active-text="开" inactive-text="关" />
         </el-form-item>
-        <!-- <el-form-item label="活动区域" prop="region">
-          <el-select v-model="ruleForm.region" placeholder="请选择活动区域">
-            <el-option label="区域一" value="shanghai" />
-            <el-option label="区域二" value="beijing" />
-          </el-select>
+        <el-form-item label="面包屑状态" prop="meta.breadcrumb">
+          <el-switch v-model="addForm.meta.breadcrumb" active-text="显示" inactive-text="不显示" />
         </el-form-item>
-
-        <el-form-item label="即时配送" prop="delivery">
-          <el-switch v-model="ruleForm.delivery" />
-        </el-form-item>
-        <el-form-item label="活动性质" prop="type">
-          <el-checkbox-group v-model="ruleForm.type">
-            <el-checkbox label="美食/餐厅线上活动" name="type" />
-            <el-checkbox label="地推活动" name="type" />
-            <el-checkbox label="线下主题活动" name="type" />
-            <el-checkbox label="单纯品牌曝光" name="type" />
-          </el-checkbox-group>
-        </el-form-item>
-        <el-form-item label="特殊资源" prop="resource">
-          <el-radio-group v-model="ruleForm.resource">
-            <el-radio label="线上品牌商赞助" />
-            <el-radio label="线下场地免费" />
-          </el-radio-group>
-        </el-form-item>
-        <el-form-item label="活动形式" prop="desc">
-          <el-input v-model="ruleForm.desc" type="textarea" />
-        </el-form-item>-->
         <el-form-item>
-          <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
-          <el-button @click="resetForm('ruleForm')">重置</el-button>
+          <el-button type="primary" @click="submitForm('addForm')">立即添加</el-button>
+          <el-button @click="resetForm('addForm')">重置</el-button>
         </el-form-item>
       </el-form>
     </el-dialog>
@@ -87,58 +66,28 @@
 </template>
 <script>
 import * as RouterMenuApi from "../../api/router_api";
+import { MessageBox, Message } from "element-ui";
 export default {
   data() {
     return {
       addFormVisible: false,
       menuList: [],
-      ruleForm: {
+      addForm: {
         name: "",
-        region: "",
-        date1: "",
-        date2: "",
-        delivery: false,
-        type: [],
-        resource: "",
-        desc: ""
+        path: "",
+        hidden: false,
+        redirect: "",
+        alwaysShow: false,
+        component: "",
+        sort: "",
+        meta: {
+          title: "",
+          icon: "",
+          noCache: false,
+          breadcrumb: true
+        }
       },
-      rules: {
-        name: [
-          { required: true, message: "请输入活动名称", trigger: "blur" },
-          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
-        ],
-        region: [
-          { required: true, message: "请选择活动区域", trigger: "change" }
-        ],
-        date1: [
-          {
-            type: "date",
-            required: true,
-            message: "请选择日期",
-            trigger: "change"
-          }
-        ],
-        date2: [
-          {
-            type: "date",
-            required: true,
-            message: "请选择时间",
-            trigger: "change"
-          }
-        ],
-        type: [
-          {
-            type: "array",
-            required: true,
-            message: "请至少选择一个活动性质",
-            trigger: "change"
-          }
-        ],
-        resource: [
-          { required: true, message: "请选择活动资源", trigger: "change" }
-        ],
-        desc: [{ required: true, message: "请填写活动形式", trigger: "blur" }]
-      }
+      rules: {}
     };
   },
   mounted: function() {
@@ -153,7 +102,23 @@ export default {
     submitForm(formName) {
       this.$refs[formName].validate(valid => {
         if (valid) {
-          alert("submit!");
+          console.info(this.addForm);
+          RouterMenuApi.addParentRouter(this.addForm).then(res => {
+            console.info(res);
+            if (res.data.code === 20000) {
+              this.$message({
+                message: "添加成功",
+                type: "success"
+              });
+              this.menuList.push(res.data.data);
+              this.addFormVisible = false;
+            } else {
+              this.$message({
+                message: "添加失败",
+                type: "error"
+              });
+            }
+          });
         } else {
           console.log("error submit!!");
           return false;
@@ -161,6 +126,7 @@ export default {
       });
     },
     resetForm(formName) {
+      this.$refs[formName].resetFields();
       this.$refs[formName].resetFields();
     }
   }
